b6a723cee45b6854512bcb53909696bdc65d867e
Stefan <github@stefan.industries>
2025-05-26 14:08:02 +0200

    Add pairing flow and integration with PetSafe API. not functioning as of now.

--- drivers/feeder/driver.ts
+++ drivers/feeder/driver.ts
@@ -1,31 +1,75 @@
 import Homey from 'homey';
+import PairSession from "homey/lib/PairSession";
+import {PetSafeClient} from "mypetsafe/lib/mypetsafe-client";
 
 module.exports = class MyDriver extends Homey.Driver {
+  psClient: PetSafeClient | undefined;
+  email: string | undefined;
+  accessToken: string | undefined;
+  idToken: string | undefined;
+  refreshToken: string | undefined;
+  session: string | undefined;
 
-  /**
-   * onInit is called when the driver is initialized.
-   */
   async onInit() {
     this.log('MyDriver has been initialized');
+    this.accessToken = this.homey.settings.get("accessToken");
+    this.email = this.homey.settings.get("email");
+    if (this.accessToken && this.email && this.accessToken !== "" && this.email !== "") {
+      this.idToken = this.homey.settings.get("idToken");
+      this.refreshToken = this.homey.settings.get("refreshToken");
+      this.session = this.homey.settings.get("session");
+      this.psClient = new PetSafeClient(this.email!, this.idToken, this.refreshToken, this.accessToken, this.session);
+    }
   }
 
-  /**
-   * onPairListDevices is called when a user is adding a device and the 'list_devices' view is called.
-   * This should return an array with the data of devices that are available for pairing.
-   */
-  async onPairListDevices() {
-    return [
-      // Example device data, note that `store` is optional
-      // {
-      //   name: 'My Device',
-      //   data: {
-      //     id: 'my-device',
-      //   },
-      //   store: {
-      //     address: '127.0.0.1',
-      //   },
-      // },
-    ];
+  async onPair(session: PairSession) {
+    this.log('starting a new pair session');
+
+    session.setHandler('showView', async (view) => {
+      if (view === 'prepare_pair') {
+        if (this.psClient && this.psClient.getAccessToken) {
+          await session.showView('list_devices');
+        } else {
+          await session.showView('email');
+        }
+      }
+    });
+
+    session.setHandler('email_submitted', async (data: {email: string}) => {
+      this.email = data.email;
+      this.log('pair: email submitted', this.email);
+      this.psClient = new PetSafeClient(this.email);
+      this.log('petsafeclient created, requesting code now');
+      await this.psClient.requestCode();
+      this.log('code requested');
+      return true;
+    });
+
+    session.setHandler("pincode", async (pincode) => {
+      this.log('pair: pincode submitted');
+      const pinCodeAsString = pincode.join("");
+      await this.psClient!.requestTokensFromCode(pinCodeAsString);
+      this.homey.settings.set("accessToken", this.psClient!.getAccessToken);
+      this.homey.settings.set("idToken", this.psClient!.getIdToken);
+      this.homey.settings.set("refreshToken", this.psClient!.getRefreshToken);
+      this.homey.settings.set("session", this.psClient!.getSession);
+      return true;
+    });
+
+    session.setHandler('list_devices', async () => {
+      this.log('pair: list_devices');
+      const foundDevices = await this.psClient!.getFeeders();
+      const mappedDevices = foundDevices.map(device => {
+        return {
+          name: device.friendlyName,
+          data: {
+            id: device.id
+          },
+          icon: 'default.svg'
+        }
+      });
+      return foundDevices;
+    });
   }
 
 };
